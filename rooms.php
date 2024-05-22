6a78c964a5349099f20a88e5b5b9580673013196
Fenet-damena <fenetdamena74@gmail.com>
2024-05-22 02:16:33 -1200

    room's features ,facilities and guests added

--- rooms.php
+++ rooms.php
@@ -22,14 +22,14 @@
 
     <div class="container">
     <div class="row">
-        <div class="col-lg-3  col-md-12 mb-lg-0 mb-4">
+<div class="col-lg-3  col-md-12 mb-lg-0 mb-4 px-lg-0">
   <nav class="navbar navbar-expand-lg bg-light bg-white rounded shadow">
-  <div class="container-fluid flex-lg-column align-items-stretch">
+   <div class="container-fluid flex-lg-column align-items-stretch">
     <h4 class="mt-2">FILTERS</h4>
     <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#filterDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse flex-column width=30px align-items-stretch mt-2" id="filterDropdown">
+     <div class="collapse navbar-collapse flex-column width=30px align-items-stretch mt-2" id="filterDropdown">
         <div class="border bg-light p-3 rounded mb-3">
             <h5 class="mb-3" style="font-size: 18px;">CHECK AVAILABILITY</h5>
             <label class="form-label">Check-in</label>
@@ -38,7 +38,7 @@
             <input type="date" class="form-control shadow-none">
 
         </div>
-        <div class="border bg-light p-3 rounded mb-3">
+         <div class="border bg-light p-3 rounded mb-3">
             <h5 class="mb-3" style="font-size: 18px;">FACILITIES</h5>
             <div class="nb-2">
             <input type="checkbox" id="f1" class="form-check-input shadow-none me-1">
@@ -57,7 +57,7 @@
             </div>
 
         </div>
-        <div class="border bg-light p-3 rounded mb-3">
+         <div class="border bg-light p-3 rounded mb-3">
             <h5 class="mb-3" style="font-size: 18px;">GUESTS</h5>
             <div class="d-flex">
             <div class="me-3">
@@ -76,11 +76,113 @@
    
     </div>
   </div>
-</nav>
-        </div>
+  </nav>
+</div>
         <div class="col-lg-9 col-md-12 px-4">
-
+<div class="card mb-4 border-0 shadow-none">
+  <div class="row g-0 p-3 align-items-center">
+    <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+      <img src="images/rooms/room_img/room1.jpg" class="img-fluid rounded">
+    </div>
+    <div class="col-md-5 px-lg-3 px-md-3 px-8">
+        <h5 class="mb-1">Simple Room Name</h5>
+        <div class="features mb-3">
+                             <h6 class="mb-1">Features</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">2 Rooms</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bathroom</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bed</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Kitchen</span>
+                         </div>
+        <div class="facilities mb-3">
+                             <h6 class="mb-1 mt-3">Facilities</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">wi fi</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Room Heater</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Television</span>
+                         </div>
+        <div class="guests mb-3">
+                             <h6 class="mb-1 mt-3">Guests</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">5 Adults</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">4 children</span></div>  </div>
+    
+    <div class="col-md-2 text-center">
+      <h6 class="mb-4">100USD per night</h6>
+      <a href="#" class="btn btn-primary text-white w-100 shadow-none mb-2">Book Now</a>
+      <a href="#" class="btn btn-sa btn-outline-dark w-100 rounded-0 fw-bold shadow-none">More
+                                 details</a>
+     
+    </div>
+  </div>
+</div>
+<div class="card mb-4 border-0 shadow-none">
+  <div class="row g-0 p-3 align-items-center">
+    <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+      <img src="images/rooms/room_img/room1.jpg" class="img-fluid rounded">
+    </div>
+    <div class="col-md-5 px-lg-3 px-md-3 px-8">
+        <h5 class="mb-1">Simple Room Name</h5>
+        <div class="features mb-3">
+                             <h6 class="mb-1">Features</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">2 Rooms</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bathroom</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bed</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Kitchen</span>
+                         </div>
+        <div class="facilities mb-3">
+                             <h6 class="mb-1 mt-3">Facilities</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">wi fi</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Room Heater</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Television</span>
+                         </div>
+        <div class="guests mb-3">
+                             <h6 class="mb-1 mt-3">Guests</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">5 Adults</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">4 children</span></div>  </div>
+    
+    <div class="col-md-2 mt-lg-0 mt-md-0 mt-4 text-center">
+      <h6 class="mb-4">100USD per night</h6>
+      <a href="#" class="btn btn-primary text-white w-100 shadow-none mb-2">Book Now</a>
+      <a href="#" class="btn btn-sa btn-outline-dark w-100 rounded-0 fw-bold shadow-none">More
+                                 details</a>
+     
+    </div>
+  </div>
+</div>
+<div class="card mb-4 border-0 shadow-none">
+  <div class="row g-0 p-3 align-items-center">
+    <div class="col-md-5 mb-lg-0 mb-md-0 mb-3">
+      <img src="images/rooms/room_img/room1.jpg" class="img-fluid rounded">
+    </div>
+    <div class="col-md-5 px-lg-3 px-md-3 px-8">
+        <h5 class="mb-1">Simple Room Name</h5>
+        <div class="features mb-3">
+                             <h6 class="mb-1">Features</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">2 Rooms</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bathroom</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">1 Bed</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Kitchen</span>
+                         </div>
+        <div class="facilities mb-3">
+                             <h6 class="mb-1 mt-3">Facilities</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">wi fi</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Room Heater</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">Television</span>
+                         </div>
+        <div class="guests mb-3">
+                             <h6 class="mb-1 mt-3">Guests</h6>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">5 Adults</span>
+                             <span class="badge rounded-pill bg-light text-dark  text-wrap">4 children</span></div>  </div>
+    
+    <div class="col-md-2 text-center">
+      <h6 class="mb-4">100USD per night</h6>
+      <a href="#" class="btn btn-primary text-white w-100 shadow-none mb-2">Book Now</a>
+      <a href="#" class="btn btn-sa btn-outline-dark w-100 rounded-0 fw-bold shadow-none">More
+                                 details</a>
+     
+    </div>
+  </div>
+</div>
         </div>
+
       
 
 
