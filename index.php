30bed6dee26014cf3bf917b14beba0ac398c06c0
72730882 <faizamohammed72730882@gmail.com>
2024-05-26 17:49:33 -1200

    correcting the history

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
  <body class="bg-light">
     <?php require('inc/header.php')?>
-     <!------ cayousel ------>
+     <!------ carousel ------>
      <div class="container-fluid px-lg-4 mt-4">
          <div class="swiper swiper-container">
              <div class="swiper-wrapper">
