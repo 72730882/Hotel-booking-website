a71721227f5dccc2a4bc727325e6138e797e8b2c
Gelilamihirke <mihirkelala@gmail.com>
2024-05-31 16:19:09 +0300

    please work

--- contact.php
+++ contact.php
@@ -26,21 +26,41 @@
         <div class="row">
             <div class="col-lg-6 col-md-6 mb-5 px-4">
                 <div class="bg-white ronded shadow p-4">
-                    <iframe class="w-100 rounded mb-4" height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126115.11523351159!2d38.7781448!3d8.96317685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa%2C%20Ethiopia!5e0!3m2!1sen!2sus!4v1715893357712!5m2!1sen!2sus" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe class="w-100 rounded mb-4" height="320px" src="<?php echo $contact_r['iframe'] ?>" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     <h5> Address</h5>
-                    <a href="https://maps.app.goo.gl/8w55vsnBmkZYVK647" target="_blank" class="d-inline-block text-decoration-none text-dark">
-                        <i class="bi bi-geo-alt"></i> megenaga,Addis Ababa,Ethiopia</a>
+                    <a href="<?php echo $contact_r['gmap'] ?>" target="_blank" class="d-inline-block text-decoration-none text-dark">
+                        <i class="bi bi-geo-alt"></i> <?php echo $contact_r['address'] ?></a>
                     <h5 class="mt-4">Call Us</h5>
-                    <a href="tel: +111 2222 333 3444" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+111 2222 333 3444</a><br>
-                    <a href="tel: +111 2222 333 3444" class="d-inline-block mb-2 text-decoration-none text-dark"><i class="bi bi-telephone-fill"></i>+111 2222 333 3444</a>
+                    <a href="tel: +<?php echo $contact_r['pn1'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <i class="bi bi-telephone-fill"></i>+<?php echo $contact_r['pn1'] ?>
+                </a><br>
+                <?php
+                    if ($contact_r['pn2']!=''){
+                        echo<<<data
+                            <a href="tel: +$contact_r[pn2]" class="d-inline-block mb-2 text-decoration-none text-dark">
+                            <i class="bi bi-telephone-fill"></i>+$contact_r[pn2]</a>
+                        data;
+                   }
+                ?>
                     <h5 class="mt-4"> Email</h5>
-                    <a href="addis: addisstay@gmail.com" class="d-inline-block mb-2 text-decoration-none text-dark">
+                    <a href="addis: <?php echo $contact_r['email'] ?>" class="d-inline-block mb-2 text-decoration-none text-dark">
                         <i class="bi bi-envelope"></i>
-                        addisstay@gmail.com</a>
+                        <?php echo $contact_r['email'] ?>
+                    </a>
+
                     <h5 class="mt-4">Follow Us</h5>
-                    <a href="#" class="d-inline-block text-dark fs-5 me-2"><i class="bi bi-twitter"></i></a>
-                    <a href="#" class="d-inline-block  text-dark fs-5 me-2"><i class="bi bi-facebook"></i></a>
-                    <a href="#" class="d-inline-block text-dark fs-5 "><i class="bi bi-instagram"></i></a>
+                    <?php
+                    if($contact_r('tw')!=''){
+                        echo<<<data
+                        <a href="$contact_r[tw]" class="d-inline-block text-dark fs-5 me-2">
+                        <i class="bi bi-twitter"></i>
+                        </a>
+                        data;
+                    }
+                    ?>
+                    
+                    <a href="<?php echo $contact_r['fb'] ?>" class="d-inline-block  text-dark fs-5 me-2"><i class="bi bi-facebook"></i></a>
+                    <a href="<?php echo $contact_r['insta'] ?>" class="d-inline-block text-dark fs-5 "><i class="bi bi-instagram"></i></a>
                 </div>
             </div>
             <div class="col-lg-6 md-6 mb-5 px-4">
